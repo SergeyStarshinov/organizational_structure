80c159d0cde1cafd512557b38a650ff2f793a539
arrowcle <arrowcle@student.21-school.ru>
2026-05-23 16:10:49 +0300

    refactor: split handler.go

---
+++ internal/web/createDepartment.go
@@ -0,0 +1,49 @@
+package web
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"orgstructure/internal/domain/model"
+	"orgstructure/internal/logger"
+	"strings"
+	"time"
+)
+
+func (h BaseHandler) CreateDepartment(w http.ResponseWriter, r *http.Request) {
+	h.log.Debug("start web.CreateDepartment")
+	w.Header().Set("Content-Type", "application/json")
+	var req departmentRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		h.log.Error("web.CreateDepartment, invalid request body:", logger.Err(err))
+		return
+	}
+	req.Name = strings.TrimSpace(req.Name)
+
+	if !h.siblingsCheck(req.Parent_id, req.Name) {
+		http.Error(w, "invalid request body: duplicate name of department", http.StatusBadRequest)
+		h.log.Error(fmt.Sprintf("web.CreateDepartment, duplicate name %s for parent_id %d:",
+			req.Name, req.Parent_id))
+		return
+	}
+
+	var newDepartment model.Department
+	newDepartment.Name = req.Name
+	parentID := req.Parent_id
+	if parentID != 0 {
+		newDepartment.ParentID = &parentID
+	}
+	newDepartment.CreatedAt = time.Now()
+	newDepartment, err := h.data.CreateDepartment(newDepartment)
+	if err != nil {
+		http.Error(w, "creating department error", http.StatusBadRequest)
+		h.log.Error("web.CreateDepartment, creating department error:", logger.Err(err))
+		return
+	}
+
+	h.log.Info(fmt.Sprintf("new department created. id: %d, name: %s, parent_id: %d",
+		newDepartment.ID, newDepartment.Name, parentID))
+	departmentInfo, _ := json.Marshal(newDepartment)
+	w.Write([]byte(departmentInfo))
+}
---
+++ internal/web/createEmployee.go
@@ -0,0 +1,64 @@
+package web
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"orgstructure/internal/domain/model"
+	"orgstructure/internal/logger"
+	"strconv"
+	"strings"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+func (h BaseHandler) CreateEmployee(w http.ResponseWriter, r *http.Request) {
+	h.log.Debug("start web.CreateEmployee")
+	w.Header().Set("Content-Type", "application/json")
+	idString := strings.TrimSpace(r.PathValue("id"))
+	departmentID, err := strconv.Atoi(idString)
+	if err != nil {
+		http.Error(w, "incorrect department id, must be a number", http.StatusBadRequest)
+		h.log.Error("web.CreateEmployee, department id isn't a number:", logger.Err(err))
+		return
+	}
+	if _, err := h.data.GetDepartment(departmentID); errors.Is(err, gorm.ErrRecordNotFound) {
+		http.Error(w, "invalid department id", http.StatusNotFound)
+		h.log.Error("web.CreateEmployee, invalid department id: "+idString, logger.Err(err))
+		return
+	}
+
+	var req employeeRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		h.log.Error("web.CreateEmployee, invalid request body:", logger.Err(err))
+		return
+	}
+
+	var newEmployee model.Employee
+	if req.Hired_at != "" {
+		hiredDate, err := time.Parse("2006-01-02", req.Hired_at)
+		if err != nil {
+			http.Error(w, "invalid date format, must be YYYY-MM-DD", http.StatusBadRequest)
+			h.log.Error("web.CreateEmployee, invalid date format:", logger.Err(err))
+			return
+		}
+		newEmployee.Hired_at = &hiredDate
+	}
+	newEmployee.DepartmentID = departmentID
+	newEmployee.FullName = strings.TrimSpace(req.Full_name)
+	newEmployee.Position = strings.TrimSpace(req.Position)
+	newEmployee.CreatedAt = time.Now()
+	newEmployee, err = h.data.CreateEmployee(newEmployee)
+	if err != nil {
+		http.Error(w, "creating employee error", http.StatusBadRequest)
+		h.log.Error("web.CreateEmployee, creating employee error:", logger.Err(err))
+		return
+	}
+	h.log.Info(fmt.Sprintf("new employee created. id %d, department id: %d, fullname: %s, position %s, hired at %v",
+		newEmployee.ID, newEmployee.DepartmentID, newEmployee.FullName, newEmployee.Position, newEmployee.Hired_at))
+	employeeInfo, _ := json.Marshal(newEmployee)
+	w.Write([]byte(employeeInfo))
+}
